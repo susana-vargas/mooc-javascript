1a8024b4c3859f9ee24af021bdcd669696175b61
Susana <hernandeznerisusanaisabel@gmail.com>
2023-03-30 15:02:48 -0600

    once-upon-a-time resolved

---
+++ once-upon-a-time/script.js
@@ -0,0 +1,15 @@
+(function () {
+    var Main = /** @class */ (function () {
+        function Main() {
+        }
+        Main.main = function () {
+            //aqui empieza tu codigo
+            console.log('once');
+            console.log('upon');
+            console.log('a');
+            console.log('time');
+        };
+        return Main;
+    }());
+    Main.main();
+})();
---
+++ once-upon-a-time/script.ts
@@ -0,0 +1,12 @@
+(() => {
+    class Main {
+      static main() {
+        //aqui empieza tu codigo
+        console.log('once')
+        console.log('upon')
+        console.log('a')
+        console.log('time')
+      }
+    }
+    Main.main();
+  })();
