e5cfe6319be6ba79f865cb73e1010cd7371f10cb
Susana <hernandeznerisusanaisabel@gmail.com>
2023-03-30 14:15:10 -0600

    ada-lovelace resolved

---
+++ ada-lovelace/script.js
@@ -0,0 +1,17 @@
+(function () {
+    var Main = /** @class */ (function () {
+        function Main() {
+        }
+        Main.main = function () {
+            //aqui empieza tu codigo
+            var name = "ada lovelace";
+            console.log(name);
+            name = "Isabel";
+            console.log(name);
+            var number = 16;
+            console.log(number);
+        };
+        return Main;
+    }());
+    Main.main();
+})();
---
+++ ada-lovelace/script.ts
@@ -0,0 +1,14 @@
+(() => {
+    class Main {
+      static main() {
+        //aqui empieza tu codigo
+        let name: string = "ada lovelace" 
+        console.log(name)
+        name = "Isabel"
+        console.log(name)
+        let number: number = 16
+        console.log(number)
+      }
+    }
+    Main.main();
+  })();
