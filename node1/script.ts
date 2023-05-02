3e418ecd06a87f74089eb5a13051cf44effd6245
Susana <hernandeznerisusanaisabel@gmail.com>
2023-05-02 11:45:58 -0600

    node1: resolved

---
+++ node1/script.ts
@@ -0,0 +1,13 @@
+class Main {
+    public main() {
+      //aqui empieza tu codigo
+     console.log('hola desde la terminal');
+     
+    }
+
+    
+  }
+  
+  const main = new Main();
+  main.main();
+
