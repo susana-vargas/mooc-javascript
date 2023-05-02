85e125f18d51ff782e6bbf5755505c15db1d85ec
Susana <hernandeznerisusanaisabel@gmail.com>
2023-05-02 11:53:39 -0600

    node2: resolved

---
+++ node2/script.ts
@@ -0,0 +1,15 @@
+class Main {
+    public main() {
+      //aqui empieza tu codigo
+      let num1: number = 13;
+      let num2: number = 30;
+     console.log(num1 + num2);
+     
+    }
+
+    
+  }
+  
+  const main = new Main();
+  main.main();
+
