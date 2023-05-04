a3e478b822d471a816dab5f4d289fea6169f104b
Susana <hernandeznerisusanaisabel@gmail.com>
2023-05-04 12:19:51 -0600

    dom-manipulation2: resolved

---
+++ dom-manipulation2/script.js
@@ -0,0 +1,4 @@
+var h2Element = document.getElementById('h2-element');
+console.log(h2Element, 'estees el elemento');
+h2Element.innerText = '¡cambiado desde typescript.';
+//# sourceMappingURL=script.js.map
---
+++ dom-manipulation2/script.ts
@@ -0,0 +1,4 @@
+const h2Element = document.getElementById('h2-element')!!;
+console.log(h2Element,'este es el elemento');
+h2Element.innerText = '¡cambiado desde typescript.';
+
