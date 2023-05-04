8e896bf88fcc704b45771cd4006d35d7ebf4d7d0
Susana <hernandeznerisusanaisabel@gmail.com>
2023-05-04 09:41:29 -0600

    node3: refactorized

--- node3/script.ts
+++ node3/script.ts
@@ -2,37 +2,30 @@ class Main {
     public main() {
       //aqui empieza tu codigo
      let begin: number = 1
-     let end: number = 10;
-    this.printStars(end)
+     let cantidad: number = 5;
+     this.printRectangle(3, 6)
     //     console.log('-'.repeat(5))
      //this.printSquare(10)
-    // this.printrectangle(3, 6)
+    // this.printRectangle(3, 6)
     } 
 
-    printStars(end: number) {
-        let i: number = 1;
-        while(i <= end) {
-            let star: string = '*'
-            console.log(star.repeat(i))
-            i++
-         }
+    printStars(quantity: number) {
+        let stars: string = '*'.repeat(quantity);
+        console.log(stars);
     }
     
-    printSquare(begin: number) {
+    printSquare(side: number) {
         let i : number = 1;
-        while(i <= begin) {
-            let star: string = '*'
-            console.log(star.repeat(begin))
+        while(i <= side) {
+            this.printStars(side)
             i++;
         }
     }
 
-
-    printrectangle(height: number, width: number) {
+    printRectangle(height: number, width: number) {
         let i: number = 1;
         while(i <= height) {
-            let star: string = '*'
-            console.log(star.repeat(width))
+            this.printStars(width)
             i++
          }
     }
