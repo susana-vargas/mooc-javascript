98a196b6f07aedd32c8400cd0fa0586fba63eb80
Susana <hernandeznerisusanaisabel@gmail.com>
2023-05-04 13:50:13 -0600

    dom-manipulation4: resolved

---
+++ dom-manipulation4/script.js
@@ -0,0 +1,6 @@
+var miBoton = document.getElementById('mi-boton');
+miBoton.addEventListener('click', onClick);
+function onClick() {
+    console.log('click');
+}
+//# sourceMappingURL=script.js.map
