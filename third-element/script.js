90b91c47c6b076274b05175d0df790c66dc90ed3
Susana <hernandeznerisusanaisabel@gmail.com>
2023-05-05 14:42:32 -0600

    third-element: resolved

---
+++ third-element/script.js
@@ -0,0 +1,16 @@
+var Main = /** @class */ (function () {
+    function Main() {
+    }
+    Main.prototype.main = function () {
+        // aqui empieza tu codigo
+        var nombres = ['susana', 'nieves', 'cristina', 'ganjah'];
+        console.log(nombres[0]);
+        console.log(nombres[1]);
+        console.log(nombres[2]);
+        console.log(nombres[3]);
+    };
+    return Main;
+}());
+var main = new Main();
+main.main();
+//# sourceMappingURL=script.js.map
