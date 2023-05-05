e590c65739587d79ba00de437814908ae2d0a80b
Susana <hernandeznerisusanaisabel@gmail.com>
2023-05-05 13:20:56 -0600

    dom-manipulaton6: resolved

---
+++ dom-manipulation6/script.js
@@ -0,0 +1,10 @@
+var encabezadoh1 = document.getElementById('encabezado-h1');
+console.log(encabezadoh1, 'soy un tiltulo');
+encabezadoh1.textContent = 'soy un titulo';
+var encabezadoh2 = document.getElementById('encabezado-h2');
+console.log(encabezadoh2, 'tengo mal el texto');
+encabezadoh2.textContent = 'tengo un texto corejido';
+var parrafo = document.getElementById('parrafo');
+console.log(parrafo, 'tengo mal el texto');
+parrafo.textContent = 'tengo otro texto coregido';
+//# sourceMappingURL=script.js.map
---
+++ dom-manipulation6/script.ts
@@ -0,0 +1,13 @@
+const encabezadoh1 = document.getElementById('encabezado-h1')!!;
+console.log(encabezadoh1,'soy un tiltulo');
+encabezadoh1.textContent = 'soy un titulo';
+
+const encabezadoh2 = document.getElementById('encabezado-h2')!!;
+console.log(encabezadoh2,'tengo mal el texto');
+encabezadoh2.textContent = 'tengo un texto corejido';
+
+const parrafo = document.getElementById('parrafo')!!;
+console.log(parrafo,'tengo mal el texto');
+parrafo.textContent = 'tengo otro texto coregido';
+
+
