e4701c67da21d729729c770a9683f7185cde1201
Susana <hernandeznerisusanaisabel@gmail.com>
2023-05-12 13:59:23 -0600

    88-index-of-smallest: resolved

--- 88-index-of-smallest/script.js
+++ 88-index-of-smallest/script.js
@@ -1,8 +1,26 @@
 var boton = document.getElementById('boton');
 boton.addEventListener('click', main);
+//aqui muestra el numero mas pequeño
 function main() {
     var numbers = leerEnteros();
-    console.log(numbers);
+    //aqui encontramos el numero mas pequeño
+    var min = numbers[0];
+    var i = 0;
+    while (i < numbers.length) {
+        var element = numbers[i];
+        if (element < min) {
+            min = element;
+        }
+        i++;
+    }
+    crearParrafoConNumero(min, 'el numero mas pequeño es ');
+    //debo saber en que indices esta ese numero
+    for (var i_1 = 0; i_1 < numbers.length; i_1++) {
+        var element = numbers[i_1];
+        if (element === min) {
+            crearParrafoConNumero(i_1, 'el numero mas pequeño esta en el indice ');
+        }
+    }
 }
 function leerEnteros() {
     var numbers = [];
@@ -16,4 +34,31 @@ function leerEnteros() {
     }
     return numbers;
 }
+function crearParrafoConNumero(numero, mensaje) {
+    var parrafo = document.createElement('p');
+    parrafo.textContent = mensaje + numero.toString();
+    document.body.appendChild(parrafo);
+}
+// dry dont repeat yourself
+// srp principio de responabilidad unica // una funcion hace una unica cosa y la hace bien
+//primero un boton **
+//obtengo el boton en typescript **
+// a lo que obtuve le añado un escuchador de eventos **
+//escribo una funcion que le pide numeros al usuario y la invoco cuando le den click al boton **
+//ya tengo un arreglo de numeros que ingreso el usuario **
+//luego en el arreglo los comparo entre si para encontrar al mas pequeño **
+//podemos hacer otro bucle que itere nuevamente el arreglo de numeros y cada vez 
+//que encuentre el numero mas pequeño añada a un arreglo el indice
+//esto nos devuelve un arreglo de numeros que reprenta los indices donde se encontro
+//n, n[]
+//invocamos una vez la funcion para mostrar el numero mas pequeño
+// iteramos el arreglo de indices con la funcion crear parrafo
+//DEBE HSER:
+//tiene que encontrar el numero pequeño 
+//despues debe mostrar los  indices en los que se encuentren los numeros pequeños 
+// EJEMPLO:
+//numero mas pequeño: 8 *
+//encontrado en el indice: 2
+//encontrado en el indice: 3
+//MOSTRAR EL INDICE DEL NUMERO MAS PEQUEÑO
 //# sourceMappingURL=script.js.map
--- 88-index-of-smallest/script.ts
+++ 88-index-of-smallest/script.ts
@@ -1,10 +1,30 @@
 const boton = document.getElementById('boton')!!;
 boton.addEventListener('click', main);
-
+//aqui muestra el numero mas pequeño
 function main() {
   const numbers: number[] = leerEnteros();
+
+  //aqui encontramos el numero mas pequeño
+  let min: number = numbers[0];
+  let i: number = 0;
+  while (i < numbers.length) {
+    const element = numbers[i];
+    if (element < min) {
+        min = element          
+    }
+    i++;
+  }  
+  crearParrafoConNumero(min, 'el numero mas pequeño es ')
+
+  //debo saber en que indices esta ese numero
+  for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
     
-    
+    if (element === min) {
+      crearParrafoConNumero(i, 'el numero mas pequeño esta en el indice ')
+    }
+  }
+  
 }
 
 function leerEnteros(): number[] {
@@ -19,3 +39,35 @@ function leerEnteros(): number[] {
     }
     return numbers;
 }
+
+function crearParrafoConNumero(numero: number, mensaje:string): void {
+    const parrafo = document.createElement('p')
+    parrafo.textContent = mensaje + numero.toString()
+    document.body.appendChild(parrafo)
+  }
+
+
+
+  // dry dont repeat yourself
+  // srp principio de responabilidad unica // una funcion hace una unica cosa y la hace bien
+
+//primero un boton **
+//obtengo el boton en typescript **
+// a lo que obtuve le añado un escuchador de eventos **
+//escribo una funcion que le pide numeros al usuario y la invoco cuando le den click al boton **
+//ya tengo un arreglo de numeros que ingreso el usuario **
+//luego en el arreglo los comparo entre si para encontrar al mas pequeño **
+//podemos hacer otro bucle que itere nuevamente el arreglo de numeros y cada vez 
+//que encuentre el numero mas pequeño añada a un arreglo el indice
+//esto nos devuelve un arreglo de numeros que reprenta los indices donde se encontro
+//n, n[]
+//invocamos una vez la funcion para mostrar el numero mas pequeño
+// iteramos el arreglo de indices con la funcion crear parrafo
+//DEBE HSER:
+//tiene que encontrar el numero pequeño 
+//despues debe mostrar los  indices en los que se encuentren los numeros pequeños 
+// EJEMPLO:
+//numero mas pequeño: 8 *
+//encontrado en el indice: 2
+//encontrado en el indice: 3
+//MOSTRAR EL INDICE DEL NUMERO MAS PEQUEÑO
