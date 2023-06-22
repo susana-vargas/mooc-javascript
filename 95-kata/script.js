e7dce09890a873c7677fcd66e35da518196adad5
Susana <hernandeznerisusanaisabel@gmail.com>
2023-06-22 15:40:02 -0600

    95-kata: arreglo de frutas : completado

---
+++ 95-kata/script.js
@@ -0,0 +1,7 @@
+console.log('hola');
+var frutas = ['manzana', 'platano', 'naranja', 'uva', 'sandia', 'piña', 'fresa', 'mango', 'melocoton', 'kiwi'];
+for (var index = 0; index < frutas.length; index++) {
+    var element = frutas[index];
+    console.log('la fruta actual es ', element);
+}
+//# sourceMappingURL=script.js.map
---
+++ 95-kata/script.ts
@@ -0,0 +1,9 @@
+console.log('hola');
+
+let frutas = ['manzana', 'platano', 'naranja', 'uva', 'sandia', 'piña', 'fresa', 'mango', 'melocoton','kiwi'];
+
+for (let index = 0; index < frutas.length; index++) {
+  const element = frutas[index];
+  console.log('la fruta actual es ',element);
+  
+}
